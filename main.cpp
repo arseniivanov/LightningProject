2f3764cef4dc667ba217698f1bdb55b07bcfac01
Arseni <arseni_mars97@hotmail.com>
2021-01-06 12:48:05 +0100

    Repo error

--- main.cpp
+++ main.cpp
@@ -215,7 +215,7 @@ float vertices[] = {
         lightingShader.setVec3("light.position",  camera.Position);
         lightingShader.setVec3("light.direction", camera.Front);
         lightingShader.setFloat("light.innerCutOff",   glm::cos(glm::radians(12.5f)));
-        lightingShader.setFloat("light.outerCutOff",   glm::cos(glm::radians(17.5f)));
+        lightingShader.setFloat("light.outerCutOff",   glm::cos(glm::radians(18.5f)));
         lightingShader.setVec3("lightPos", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
 
